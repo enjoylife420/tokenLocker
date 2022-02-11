a1688e7dd3acde1dba057893a6daa9e259bff14b
enjoy_life <62603922+enjoylife420@users.noreply.github.com>
2022-02-11 22:37:56 +0300

    change to mainnet

--- public/constant.js
+++ public/constant.js
@@ -1,9 +1,9 @@
-const lockerAddress = '0xe7b5c1afffe4297ade4527c41ca9800c7905e7eb';
-const provider = 'https://api.avax-test.network/ext/bc/C/rpc';
+const lockerAddress = '0x8ba74905c9Ab0aa185e04498E2F83F8CDEc20561';
+const provider = 'https://api.avax.network/ext/bc/C/rpc';
 const liquidityList = [
-    'Hurricane Liquidity'
+    'TraderJoe Liquidity'
 ]
-const wavax = '0xd00ae08403b9bbb9124bb305c09058e32c39a48c';
+const wavax = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7';
 // const swapTokenLockerFactory = '0x6a33a15d826e59792b60dec950e8af2fa7d58f9e';
 const swapTokenLockerFactory = '0x74818F8D9C0666ab332Af6B8DCBc448e1F10A133';
 
