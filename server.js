ed26b18ba1ab6664453de21b1af5b5d6725d8784
enjoy_life <62603922+enjoylife420@users.noreply.github.com>
2022-02-11 18:01:23 +0300

    urlencoded to flase

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { startVesting } = require('./src/vesting');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true
+    extended: false
 }));
 
 app.use(express.static('build'));
