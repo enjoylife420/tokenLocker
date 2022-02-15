0f73d606610cd462e0d3667596f03cec9bf88875
enjoy_life <62603922+enjoylife420@users.noreply.github.com>
2022-02-15 21:22:18 +0300

    add axios await

--- src/statistics.js
+++ src/statistics.js
@@ -22,29 +22,28 @@ const fetchLogs = async function(interval) {
 const _fetchLogs = () => {
     if (isFetchingLogs) return;
     isFetchingLogs = true;
-    getLastLog((log) => {
+    getLastLog(async (log) => {
         const lastLog = log[0];
         let lastBlock = 0;
         if (lastLog) lastBlock = Number(lastLog.block_number);
         // console.log(lastBlock);
         let offset = 0;
         try {
-            axios.get(`${serverUrl}/${tokenAddress}/logs?chain=${chain}&from_block=${lastBlock+1}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&oder=asc`, apiConfig).then(async (response) => {
-                let logs = response.data.result;
-                const total = response.data.total;
-                const pages = Math.ceil(total / 500);
-                let page = 1;
-                while (page < pages) {
-                    offset = page * 500;
-                    const response = await axios.get(`${serverUrl}/${tokenAddress}/logs?chain=${chain}&from_block=${lastBlock+1}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&offset=${offset}&oder=asc`, apiConfig);
-                    // console.log(response.data.result.length);
-                    logs = logs.concat(response.data.result);
-                    page++;
-                }
-                AddLogs(logs, () => {
-                    isFetchingLogs = false;
-                });
-            })
+            let response = await axios.get(`${serverUrl}/${tokenAddress}/logs?chain=${chain}&from_block=${lastBlock+1}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef`, apiConfig);
+            let logs = response.data.result;
+            const total = response.data.total;
+            const pages = Math.ceil(total / 500);
+            let page = 1;
+            while (page < pages) {
+                offset = page * 500;
+                response = await axios.get(`${serverUrl}/${tokenAddress}/logs?chain=${chain}&from_block=${lastBlock+1}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&offset=${offset}`, apiConfig);
+                // console.log(response.data.result.length);
+                logs = logs.concat(response.data.result);
+                page++;
+            }
+            AddLogs(logs, () => {
+                isFetchingLogs = false;
+            });
         } catch (e) {
             isFetchingLogs = false;
         }
@@ -56,49 +55,6 @@ function filterLogs (_walletAddress, cb) {
         cb(logs);
     })
     // return logs.filter(each => getAddress(each.topic1) === _walletAddress.toLowerCase() || getAddress(each.topic2) === _walletAddress.toLowerCase());
-}
-
-async function calculateBuySell(logs, _chain, _tokenAddress, _walletAddress) {
-    let buy = 0, sell = 0, isFee = true;
-    if (await isExcludedFromFee(_chain, _tokenAddress, _walletAddress)) isFee = false;
-    logs.map(each => {
-        if (getAddress(each.topic1) === _walletAddress.toLowerCase()) {
-            if (isFee) {
-                sell += getValue(each.data) / 97 * 100;
-            } else {
-                sell += getValue(each.data);
-            }
-        } else buy += getValue(each.data);
-    })
-    let result = {buy: buy, sell: sell};
-    return result;
-}
-
-function getTopicFromAddress(topic) {
-    if (topic === null || topic === '') return '';
-    return `0x${topic.substring(topic.length - 40)}`;
-}
-
-const isExcludedFromFee = async function (_chain, _tokenAddress, _walletAddress) {
-    const isExcludedFromFeeAbi = [{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"isExcludedFromFee","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}];
-    return await runContractFunction(_chain, _tokenAddress, 'isExcludedFromFee', isExcludedFromFeeAbi, {account:_walletAddress});
-}
-
-const runContractFunction = async function (_chain, _contractAddress, _functionName, _abi, _params) {
-    let result;
-    try {
-        result = await axios.post(`${serverUrl}/${_contractAddress}/function?chain=${_chain}&function_name=${_functionName}`, {
-            "abi": _abi,
-            "params": _params
-        }, apiConfig);
-    } catch (e) {
-        result = null;
-    }
-    return result ? result.data : result;
-}
-
-function getValue(topic) {
-    return parseInt(topic, 16);
 }
 
 module.exports = {
