d183cd0a6b84252a4bdb0f4b66dab8b26e1ad5da
enjoy_life <62603922+enjoylife420@users.noreply.github.com>
2022-03-08 23:17:04 +0300

    add https filter

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next();
+    }
+});
 app.use(express.static('build'));
 
 // use Routes
